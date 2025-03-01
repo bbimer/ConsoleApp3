415b38ddec5e70e0b0baa34d7e7257f88740f346
User <asphaltlev@gmail.com>
2025-03-01 14:53:44 +0200

    добавлены классы наследники

--- ConsoleApp3/GFigure.cs
+++ ConsoleApp3/GFigure.cs
@@ -6,10 +6,59 @@ using System.Threading.Tasks;
 
 namespace GClass
 {
-    abstract class Shape
+    abstract class GFigure
     {
         public string Name { get; set; }
 
+        public abstract double FArea();
+        public abstract double FPerimeter();
+
+        void print()
+        {
+            Console.WriteLine($"Figure: {Name}");
+            Console.WriteLine($"Area: {FArea}");
+            Console.WriteLine($"Perimeter: {FPerimeter}\n");
+        }
+    }
+
+    // Трикутник, Квадрат, Ромб, Прямокутник, Паралелограм, Трапеція, Коло, Еліпс
+    class Triangle : GFigure
+    {
+
+    }
+
+    class Cube : GFigure
+    { 
+    
+    }
+
+    class Rhombus : GFigure
+    {
+    
+    }
+
+    class Rectangular : GFigure
+    {
+
+    }
+
+    class Parallelogram : GFigure
+    {
+
+    }
+
+    class Trapezoid : GFigure
+    {
+
+    }
+
+    class Circle : GFigure
+    {
+
+    }
+
+    class Ellipse : GFigure
+    {
 
     }
 }
